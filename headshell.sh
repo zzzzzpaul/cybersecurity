3544a4ac46d8e81595494a71b202c023d3826a46
zzzzzpaul <zzzzzpaul@qq.com>
2021-03-01 14:38:15 +0800

    updated some

---
+++ headshell.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "+--------------------------o00o" &&\
+echo "|                  (Hello,    |" &&\
+echo "|       __[¯]__   /  I'm      |" &&\
+echo "|        (o o)    coder Paul. |" &&\
+echo "|         |~|      Think and  |" &&\
+echo "|       _/===\_     Try to    |" &&\
+echo "|      // o=o ＼＼    Answer  |" &&\
+echo "|     <] |o=o|   [>    my     |" &&\
+echo "|        \===/      questions.|" &&\
+echo "|        Ii  I                |" &&\
+echo "|       /I   I\               |" &&\
+echo "|       ||   ||               |" &&\
+echo "|      <_|   |_>              |" &&\
+echo "|  Why do people believe in   |" &&\
+echo "|  ghosts?                    |" &&\
+echo "|  Why do people believe that |" &&\
+echo "|  soul exists?               |" &&\
+echo "|  Why don't people believe in|" &&\
+echo "|  god?                       |" &&\
+echo "o00o--------------------------+"
